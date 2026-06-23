25c98814b6338ae7001e4f10511652108e2ad464
Afaf Naser <afafnaser226@gmail.com>
2026-06-23 00:17:43 -0700

    Add application repositories and services interfaces

---
+++ AirportTicketBookingSystem.Application/Abstractions/Repositories/IBookingRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AirportTicketBookingSystem.Domain.Entities;
+
+namespace AirportTicketBookingSystem.Application.Abstractions.Repositories
+{
+    public interface IBookingRepository
+    {
+        Task<IReadOnlyList<Booking>> GetAllAsync();
+        Task<Booking?> GetByIdAsync(Guid id);
+        Task AddAsync(Booking booking);
+        Task UpdateAsync(Booking booking);
+    }
+}
---
+++ AirportTicketBookingSystem.Application/Abstractions/Services/ICurrentUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AirportTicketBookingSystem.Domain.Enums;
+
+namespace AirportTicketBookingSystem.Application.Abstractions.Services
+{
+    public interface ICurrentUserService
+    {
+        Guid? UserId { get; }
+        string? Email { get; }
+        UserRole? Role { get; }
+        bool IsAuthenticated { get; }
+
+        void SignIn(Guid userId, string email, UserRole role);
+        void SignOut();
+    }
+}
