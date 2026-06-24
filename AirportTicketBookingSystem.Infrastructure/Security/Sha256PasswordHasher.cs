d70148bc7cac6e183f29be101f490d1d4667b8d7
Afaf Naser <afafnaser226@gmail.com>
2026-06-23 22:19:31 -0700

    Add password hasher

---
+++ AirportTicketBookingSystem.Infrastructure/Security/Sha256PasswordHasher.cs
@@ -0,0 +1,31 @@
+﻿using AirportTicketBookingSystem.Application.Abstractions.Services;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AirportTicketBookingSystem.Infrastructure.Security
+{
+    public sealed class Sha256PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(password);
+
+            var bytes = Encoding.UTF8.GetBytes(password);
+            var hashBytes = SHA256.HashData(bytes);
+
+            return Convert.ToHexString(hashBytes);
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(password);
+            ArgumentException.ThrowIfNullOrWhiteSpace(passwordHash);
+
+            var computedHash = Hash(password);
+
+            return string.Equals(computedHash, passwordHash, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
